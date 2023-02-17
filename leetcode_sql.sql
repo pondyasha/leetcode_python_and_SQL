b37bc4d7ed7b1028d5028dc330bf36c0ef820119
asrpondi <48274072+pondyasha@users.noreply.github.com>
2023-02-17 12:46:48 -0500

    550. Game Play Analysis IV
    solved

--- leetcode_sql.sql
+++ leetcode_sql.sql
@@ -73,5 +73,40 @@ group by 1,2
 | 3         | 2016-03-02 | 0                   |
 | 3         | 2018-07-03 | 5                   |
 
+-----------------------------------------------------------------------------------------------------------------------------
+/* 550. Game Play Analysis IV
+ +--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
 
+
+Write an SQL query to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+
+The query result format is in the following example.
+ */
+
+--Write your MySQL query statement below
+select ifnull(round(count(a.player_id)/a.count_players,2),0) as fraction
+from
+(
+select player_id,
+(select count(distinct player_id) from Activity) as count_players,
+event_date as next_day,
+min(event_date) over (partition by player_id) as first_day
+from Activity
+) a
+where (a.next_day - a.first_day) = 1
+
+-- output
+| fraction |
+| -------- |
+| 0.12     |
 
