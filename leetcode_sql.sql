4e0fafe9da587b05aa5c6ab89408b3ac73d0a017
asrpondi <48274072+pondyasha@users.noreply.github.com>
2023-03-07 19:03:07 -0500

    626. Exchange Seats
    Solved

--- leetcode_sql.sql
+++ leetcode_sql.sql
@@ -204,7 +204,42 @@ where free and previous or free and next
 | 4       |
 | 5       |
 
----------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------------
+
+/*
+ +-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the name and the ID of a student.
+id is a continuous increment.
+
+Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+The query result format is in the following example.
+ */
+
+with cte as (
+
+select  id, mod(id,2) as mod_val,
+student,
+lead(student) over (order by id asc) as lead_student,
+lag(student) over (order by id asc) as lag_student
+from Seat
+)
+
+select id,
+case when id = (select max(id) from Seat) and id%2 = 1 then student
+     when mod_val = 1 then lead_student
+     when mod_val = 0 then lag_student
+     end as student
+from cte
 
 
 
