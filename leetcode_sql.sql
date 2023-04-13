b70fc1f432890de2d7b88034a60cdf351842af95
asrpondi <48274072+pondyasha@users.noreply.github.com>
2023-04-13 19:28:31 -0400

    Update leetcode_sql.sql

--- leetcode_sql.sql
+++ leetcode_sql.sql
@@ -243,8 +243,43 @@ from cte
 
 
 
+---------------------------------------------------------------------------------------------------------------------
+/*Your team at JPMorgan Chase is soon launching a new credit card. You are asked to estimate how many cards you'll issue in the first month.
 
+Before you can answer this question, you want to first get some perspective on how well new credit card launches typically do in their first month.
 
+Write a query that outputs the name of the credit card, and how many cards were issued in its launch month. The launch month is the earliest record in the monthly_cards_issued table for a given card. Order the results starting from the biggest issued amount.
+
+monthly_cards_issued Table:
+Column Name	Type
+issue_month	integer
+issue_year	integer
+card_name	string
+issued_amount	integer
+monthly_cards_issued Example Input:
+issue_month	issue_year	card_name	issued_amount
+1	2021	Chase Sapphire Reserve	170000
+2	2021	Chase Sapphire Reserve	175000
+3	2021	Chase Sapphire Reserve	180000
+3	2021	Chase Freedom Flex	65000
+4	2021	Chase Freedom Flex	70000
+Example Output:
+card_name	issued_amount
+Chase Sapphire Reserve	170000
+Chase Freedom Flex	65000
+*/
+SELECT a.card_name, a.issued_amount
+FROM
+(
+SELECT *,
+make_date(issue_year,issue_month, 1) as Date_,
+ROW_NUMBER() OVER (PARTITION BY card_name ORDER BY make_date(issue_year,issue_month, 1) ASC) as rnk
+FROM monthly_cards_issued
+) a
+WHERE a.rnk = 1
+ORDER BY 2 DESC
+;
+----------------------------------------------------------------------------------------------------------------------
 
 
 
